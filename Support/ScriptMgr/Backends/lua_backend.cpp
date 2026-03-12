9fc1c912389f889bf243165bf23418bb9df5c0c4
GameSpy <54669564+gabengaGamer@users.noreply.github.com>
2026-03-12 17:21:22 +0100

    Lua 5.5.0

--- Support/ScriptMgr/Backends/lua_backend.cpp
+++ Support/ScriptMgr/Backends/lua_backend.cpp
@@ -12,9 +12,9 @@
 
 extern "C"
 {
-#include "3rdParty\Lua\5.4.8\lua.h"
-#include "3rdParty\Lua\5.4.8\lualib.h"
-#include "3rdParty\Lua\5.4.8\lauxlib.h"
+#include "3rdParty\Lua\5.5.0\lua.h"
+#include "3rdParty\Lua\5.5.0\lualib.h"
+#include "3rdParty\Lua\5.5.0\lauxlib.h"
 }
 
 #include "x_files.hpp"
